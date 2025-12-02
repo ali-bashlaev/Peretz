a858328147342a36b4c30cc1c89f50b6179958e1
Ali <aelan0560@gmail.com>
2025-12-02 19:24:02 +0300

    added OTP fragment xml

--- app/src/main/java/com/example/peretz/MainActivity.kt
+++ app/src/main/java/com/example/peretz/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, AuthorizationFragment())
+                .replace(R.id.fragment_container, OtpFragment())
                 .commit()
         }
     }
